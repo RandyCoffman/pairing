952bef9afdfd52a43fef154e277fad4fe2a52792
RandyCoffman <randy.coffman@wvjc.edu>
2018-03-01 11:39:34 -0500

    added tests and functions to get look at odd numbers of people. All tests are passing

--- random_pairs.rb
+++ random_pairs.rb
@@ -1,5 +1,11 @@
 def random_pairs(names)
 	pairs = names.shuffle.each_slice(2).to_a
+	if pairs.length % 2 == 1
+		pairs.pop
+		pairs
+	else 
+		pairs
+	end
 	pairs
 end
 
--- random_pairs_testing.rb
+++ random_pairs_testing.rb
@@ -11,12 +11,24 @@ class Random_pairs_test < Minitest::Test
         assert_equal([], random_pairs([]))
     end
 
-	def test_1_pair
-		assert_equal(1, random_pairs(["scott", "bob"]).length)
+	def test_4_pair
+		assert_equal(4, random_pairs(["scott", "bob", "jill", "peggy", "billybob", "joe", "johnny", "sue"]).length)
 	end
 
 	def test_2_pairs
 		assert_equal(2, random_pairs(["scott", "bob", "jack", "jill"]).length)
+	end
+
+	def test_2_pairs_with_odd_number_people
+		assert_equal(2, random_pairs(["scott", "bob", "jack", "jill", "peggy"]).length)
+	end
+
+	def test_3_pairs_with_odd_number_people
+		assert_equal(3, random_pairs(["scott", "bob", "jack", "jill", "peggy", "sue", "billybob"]).length)
+	end
+
+	def test_4_pairs_with_odd_number_people
+		assert_equal(4, random_pairs(["scott", "bob", "jill", "peggy", "billybob", "joe", "johnny", "sue", "john"]).length)
 	end
 
 end

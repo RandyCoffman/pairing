61566eb8f7470d3b5a840175a0544f0b9b0867b7
RandyCoffman <randy.coffman@wvjc.edu>
2018-03-29 12:08:31 -0400

    final project

--- app.rb
+++ app.rb
@@ -1,8 +1,61 @@
 require "sinatra"
+require_relative "random_pairs.rb"
+enable :sessions
 
 get "/" do
     erb :page_1
 end
 
 post "/keep" do
+    session[:liked_pairs] = []
+    total_names = params[:names]
+    names = total_names[0].values
+    session[:pairs] = random_pairs(names)
+    "this is an array #{session[:pairs]}"
+    redirect "/list_of_pairs"
+end
+
+get "/list_of_pairs" do
+    session[:pairs]
+    erb :page_2, locals:{pairs: session[:pairs]}
+end
+
+post "/redo" do
+    session[:pair] = params[:liked_pairs]
+    p session[:pair]
+    session[:hated_pairs] = []
+    session[:pair].each do |pairs|
+        session[:hated_pairs].push(pairs.split(','))
+    end
+    p session[:hated_pairs]
+    redirect "/done_yet"
+end
+
+get "/done_yet" do 
+    session[:pair]
+    session[:pairs]
+
+    leftover_names = Array.new
+    leftover_names = session[:pairs] - session[:hated_pairs]
+    leftover_names = leftover_names.flatten
+    session[:liked_pairs] << leftover_names
+    if leftover_names.size == 0
+        redirect "/finished"
+    else
+        session[:pairs] = random_pairs(session[:hated_pairs].flatten)
+        redirect "/list_of_pairs"
+    end
+end
+
+post "/done" do
+    session[:pairs].each do |pairs|
+        session[:liked_pairs] << pairs
+    end
+    redirect "/finished"
+end
+
+get "/finished" do
+    session[:liked_pairs]
+    p session[:liked_pairs]
+    erb :final_page, locals:{liked_pairs: session[:liked_pairs]}
 end
--- random_pairs.rb
+++ random_pairs.rb
@@ -1,22 +1,12 @@
 def random_pairs(names)
-	my_array = names.shuffle.each_slice(2).to_a 
-	if my_array.length % 2 == 0
-		my_array
-	else
-		my_array[my_array.length - 2] << my_array[my_array.length - 1]
-		my_array[my_array.length - 2].flatten!
-		my_array.pop
-		my_array
-	end
-	results = ""
-	my_array.each { |name|
-		if name.include?(name[2])
-			results << name[0] + ", " + name[1] + ", and " + name[2] + "<br>"
-        else
-            results << name[0] + ", " + name[1] + "<br>"
-        end
-	}
-	results
+	sliced_pairs = names.shuffle.each_slice(2).to_a
+    if names.length % 2 == 0
+    else
+      odd_num_names = sliced_pairs.pop
+      # p odd_num_names
+      odd_num_names = odd_num_names[0]
+      # p odd_num_names
+      sliced_pairs[0] << odd_num_names
+    end
+    sliced_pairs
 end
-
-print random_pairs(["hello", "bye", "scott", "ketchup"])
